467466ab681dac0aeab0f3c3723bcdd81949b1cf
Petr Marek <contact@petrmarek.eu>
2022-05-17 14:24:35 +0200

    add blank _main_top.slim to orders/edit cell

--- app/controllers/concerns/boutique/application_controller_base.rb
+++ app/controllers/concerns/boutique/application_controller_base.rb
@@ -4,4 +4,13 @@ module Boutique::ApplicationControllerBase
   extend ActiveSupport::Concern
 
   include Boutique::CurrentOrder
+
+  included do
+    before_action :disable_application_breadcrumbs
+  end
+
+  private
+    def disable_application_breadcrumbs
+      @hide_breadcrumbs = true
+    end
 end
