4269ddc91954af59328c85356d46e94402037655
dedekm <dedekmm@gmail.com>
2022-07-15 15:00:53 +0200

    fix subscription_frequency migration

--- db/migrate/20220712120944_add_subscription_frequency_to_products.rb
+++ db/migrate/20220712120944_add_subscription_frequency_to_products.rb
@@ -5,7 +5,7 @@ class AddSubscriptionFrequencyToProducts < ActiveRecord::Migration[7.0]
     add_column :boutique_products, :subscription_frequency, :string
 
     unless reverting?
-      Boutique::Product::Subscription.update_all(subscription_frequency: Boutique::Product::SUBSCRIPTION_FREQUENCY_OPTIONS.first)
+      Boutique::Product::Subscription.update_all(subscription_frequency: Boutique::Product::Subscription::SUBSCRIPTION_FREQUENCIES.keys.first)
     end
   end
 end
