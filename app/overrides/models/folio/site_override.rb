cd48791bab55b10cf7f83bfde0da8aedf9a6b35d
dedekm <dedekmm@gmail.com>
2022-12-15 11:35:46 +0100

    add has_many :products to site

--- app/overrides/models/folio/site_override.rb
+++ app/overrides/models/folio/site_override.rb
@@ -4,6 +4,9 @@ already_loaded = Folio::Site.method_defined?(:billing_enabled?)
 return if already_loaded
 
 Folio::Site.class_eval do
+  has_many :products, class_name: "Boutique::Product",
+                      dependent: :nullify
+
   validates :billing_name,
             :billing_address_line_1,
             :billing_address_line_2,
