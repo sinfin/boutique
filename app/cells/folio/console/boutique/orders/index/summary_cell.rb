619819654f910447b45d4b6d4900b1a4f73c1543
dedekm <dedekmm@gmail.com>
2023-02-24 14:50:08 +0100

    add paid info to console orders index summary

--- app/cells/folio/console/boutique/orders/index/summary_cell.rb
+++ app/cells/folio/console/boutique/orders/index/summary_cell.rb
@@ -2,21 +2,35 @@
 
 class Folio::Console::Boutique::Orders::Index::SummaryCell < Folio::ConsoleCell
   def show
-    render if orders.present?
+    render if orders_confirmed.present?
   end
 
-  def orders
+  def orders_confirmed
     return @orders unless @orders.nil?
 
     date = model[:date].to_date
-    @orders = model[:scope].select { |o| o.confirmed_at.try(:to_date) == date }
+    @orders_confirmed = model[:scope].select { |o| o.confirmed_at.try(:to_date) == date }
   end
 
-  def total_price
-    @total_price ||= orders.sum { |o| o.total_price.to_f }
+  def total_price_confirmed
+    @total_price ||= orders_confirmed.sum { |o| o.total_price.to_f }
   end
 
-  def average_price
-    total_price / orders.size
+  def average_price_confirmed
+    total_price_confirmed / orders_confirmed.size
+  end
+
+  def orders_paid
+    return @orders_paid unless @orders_paid.nil?
+
+    @orders_paid = orders_confirmed.select { |o| o.paid_at.present? }
+  end
+
+  def total_price_paid
+    @total_price_paid ||= orders_paid.sum { |o| o.total_price.to_f }
+  end
+
+  def average_price_paid
+    total_price_paid / orders_paid.size
   end
 end
