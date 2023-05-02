3820cdd506a7acf0d1442d4e557a154515d99d79
Petr Marek <contact@petrmarek.eu>
2023-05-02 11:27:34 +0200

    don't hide breadcrumbs in boutique controllers

--- app/controllers/boutique/application_controller.rb
+++ app/controllers/boutique/application_controller.rb
@@ -2,11 +2,4 @@
 
 class Boutique::ApplicationController < Boutique.config.parent_controller.constantize
   include Boutique::ApplicationControllerBase
-
-  before_action :disable_application_breadcrumbs
-
-  private
-    def disable_application_breadcrumbs
-      @hide_breadcrumbs = true
-    end
 end
