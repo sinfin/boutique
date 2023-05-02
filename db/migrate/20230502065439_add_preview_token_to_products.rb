d3611f9b3209e16e9a556628ac8bac745b066f3c
Petr Marek <contact@petrmarek.eu>
2023-05-02 09:15:15 +0200

    add reset_preview_token! to products migration

--- db/migrate/20230502065439_add_preview_token_to_products.rb
+++ db/migrate/20230502065439_add_preview_token_to_products.rb
@@ -3,5 +3,11 @@
 class AddPreviewTokenToProducts < ActiveRecord::Migration[7.0]
   def change
     add_column :boutique_products, :preview_token, :string
+
+    unless reverting?
+      say_with_time "Setting preview tokens" do
+        Boutique::Product.find_each(&:reset_preview_token!)
+      end
+    end
   end
 end
