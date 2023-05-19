45257d97551cfec74501e31f4f4b154972a34288
dedekm <dedekmm@gmail.com>
2023-05-19 14:42:09 +0200

    localize checkout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
 Boutique::Engine.routes.draw do
-  scope constraints: Boutique.config.checkout_routes_constraints do
-    resource :checkout, only: [], controller: :checkout do
-      get :cart
-      get :refreshed_cart
-      get :crossdomain_add_item, path: "crossdomain_add_item/:product_slug"
-      post :add_item, path: "add_item/:product_slug"
-      delete :remove_item, path: "remove_item/:line_item_id"
-      post :confirm
-      post :apply_voucher
-    end
-  end
-
-  resources :orders, only: %i[show], path: "order" do
-    member do
-      post :payment
-    end
-  end
-
   localized do
+    scope constraints: Boutique.config.checkout_routes_constraints do
+      resource :checkout, only: [], controller: :checkout do
+        get :cart
+        get :refreshed_cart
+        get :crossdomain_add_item, path: "crossdomain_add_item/:product_slug"
+        post :add_item, path: "add_item/:product_slug"
+        delete :remove_item, path: "remove_item/:line_item_id"
+        post :confirm
+        post :apply_voucher
+      end
+    end
+
     resources :products, only: %i[show]
+
+    resources :orders, only: %i[show], path: "order" do
+      member do
+        post :payment
+      end
+    end
   end
 
   get "invoice/:secret_hash", to: "invoices#show", as: :invoice
