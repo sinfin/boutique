71c7761471fe9f41e48af789c0c0ed6d01acafff
Vlada <vladislav.trefil@protonmail.com>
2023-08-09 17:11:47 +0200

    tweak payment methods style

--- app/cells/boutique/orders/payment_methods_cell.rb
+++ app/cells/boutique/orders/payment_methods_cell.rb
@@ -28,18 +28,23 @@ class Boutique::Orders::PaymentMethodsCell < Boutique::ApplicationCell
         title: Boutique::Payment.payment_method_to_human(pm),
         value: pm,
         disabled: recurrence_required? ? RECURRENT_PAYMENT_METHODS.exclude?(pm) : false,
-        enabled_for_recurrent: RECURRENT_PAYMENT_METHODS.include?(pm)
+        enabled_for_recurrent: RECURRENT_PAYMENT_METHODS.include?(pm),
+        icon_path: icon_path(pm),
       }
     end
   end
 
-  def payment_button(f, method, i)
-    f.button :submit,
-             method[:title],
-             class: "btn btn-#{i.zero? ? "primary" : "secondary"} btn-xs-block b-orders-payment-methods__submit-btn",
-             data: { payment_method: method[:value], enabled_for_recurrent: method[:enabled_for_recurrent].to_s  },
-             style: ("display:none;" if method[:value] == "APPLE_PAY"),
-             disabled: method[:disabled]
+  def icon_path(method)
+    case method
+    when "BANK_ACCOUNT"
+      "boutique/icons/bank.svg"
+    when "GPAY"
+      "boutique/icons/google-pay.svg"
+    when "APPLE_PAY"
+      "boutique/icons/apple.svg"
+    else
+      "boutique/icons/credit-card.svg"
+    end
   end
 
   def recurrence_required?
