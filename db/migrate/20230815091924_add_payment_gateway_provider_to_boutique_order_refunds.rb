dd7d8115f946feb483d1c48f68197fbbbab0d8a3
Petr Mlčoch <foton@centrum.cz>
2023-08-15 15:54:40 +0200

    removed filling of order_refund.payment_provider from migration

--- db/migrate/20230815091924_add_payment_gateway_provider_to_boutique_order_refunds.rb
+++ db/migrate/20230815091924_add_payment_gateway_provider_to_boutique_order_refunds.rb
@@ -1,9 +1,5 @@
 class AddPaymentGatewayProviderToBoutiqueOrderRefunds < ActiveRecord::Migration[7.0]
   def change
     add_column :boutique_order_refunds, :payment_gateway_provider, :string
-
-    Boutique::OrderRefund.find_each do |orf|
-      orf.save!
-    end
   end
 end
