166e12bf8b816c807f5f4255e04d6e37ce457fa9
dedekm <dedekmm@gmail.com>
2023-08-22 09:40:02 +0200

    run guard

--- db/migrate/20230815091924_add_payment_gateway_provider_to_boutique_order_refunds.rb
+++ db/migrate/20230815091924_add_payment_gateway_provider_to_boutique_order_refunds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddPaymentGatewayProviderToBoutiqueOrderRefunds < ActiveRecord::Migration[7.0]
   def change
     add_column :boutique_order_refunds, :payment_gateway_provider, :string
--- db/migrate/20230818064642_add_amount_to_boutique_payments.rb
+++ db/migrate/20230818064642_add_amount_to_boutique_payments.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddAmountToBoutiquePayments < ActiveRecord::Migration[7.0]
   def change
     add_column :boutique_payments, :amount, :decimal, precision: 10, scale: 2, null: true
