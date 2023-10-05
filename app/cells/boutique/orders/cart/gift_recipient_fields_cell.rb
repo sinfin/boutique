615ec44adfe36fbc683f7f539ed6892daf5c4e14
dedekm <dedekmm@gmail.com>
2023-10-05 15:21:19 +0200

    fix gift_recipient_notification_scheduled_for input value

--- app/cells/boutique/orders/cart/gift_recipient_fields_cell.rb
+++ app/cells/boutique/orders/cart/gift_recipient_fields_cell.rb
@@ -6,10 +6,10 @@ class Boutique::Orders::Cart::GiftRecipientFieldsCell < Boutique::ApplicationCel
   end
 
   def gift_recipient_notification_scheduled_for_input
-    date = f.object.gift_recipient_notification_scheduled_for || 90.minutes.from_now.beginning_of_hour
+    datetime = f.object.gift_recipient_notification_scheduled_for || 90.minutes.from_now.beginning_of_hour
 
     f.input :gift_recipient_notification_scheduled_for,
             required: true,
-            input_html: { value: date.strftime("%d. %m. %Y %H:%M") }
+            input_html: { value: datetime.strftime("%d. %m. %Y, %H:%M") }
   end
 end
