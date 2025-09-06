62721bdc22aec25b2deef5443803741b5a0b8958
Martin Dedek <dedekmm@gmail.com>
2025-09-06 23:40:05 +0200

    feat(seo): products/show renders template with meta tags for social media crawlers

--- app/controllers/boutique/products_controller.rb
+++ app/controllers/boutique/products_controller.rb
@@ -2,11 +2,46 @@
 
 class Boutique::ProductsController < Boutique::ApplicationController
   def show
-    permitted_params = params.permit(permitted_params_keys).to_h.symbolize_keys
-    redirect_to crossdomain_add_order_url(permitted_params.merge(product_slug: params[:id]))
+    @product = Boutique::Product.friendly.find(params[:id])
+
+    # If this is a social media crawler, render the template with meta tags
+    if crawler_request?
+      set_meta_variables(@product)
+      @og_title = @product.og_title.presence || @product.title
+      @og_image = @product.og_image.thumb(Folio::OG_IMAGE_DIMENSIONS).url if @product.og_image
+
+      render :show
+    else
+      # For regular users, redirect to cart as before
+      permitted_params = params.permit(permitted_params_keys).to_h.symbolize_keys
+      redirect_to crossdomain_add_order_url(permitted_params.merge(product_slug: params[:id]))
+    end
   end
 
   private
+    def crawler_request?
+      user_agent = request.user_agent.to_s.downcase
+
+      # Check for common social media crawlers
+      social_crawlers = [
+        "facebookexternalhit",   # Facebook
+        "twitterbot",            # Twitter/X
+        "linkedinbot",           # LinkedIn
+        "whatsapp",              # WhatsApp
+        "slackbot",              # Slack
+        "telegrambot",           # Telegram
+        "discordbot",            # Discord
+        "skypebot",              # Skype
+        "pinterest",             # Pinterest
+        "googlebot",             # Google
+        "bingbot",               # Bing
+        "applebot",              # Apple (for iMessage previews)
+        "redditbot"              # Reddit
+      ]
+
+      social_crawlers.any? { |crawler| user_agent.include?(crawler) }
+    end
+
     def permitted_params_keys
       [:subscription_id, Boutique::OrdersController::VOUCHER_GET_PARAM_NAME] + additional_permitted_params_keys
     end
--- test/controllers/boutique/products_controller_test.rb
+++ test/controllers/boutique/products_controller_test.rb
@@ -3,7 +3,7 @@
 require "test_helper"
 
 class Boutique::ProductsControllerTest < Boutique::ControllerTest
-  test "show" do
+  test "show - user gets redirected to cart" do
     product = create(:boutique_product)
 
     assert_equal 0, Boutique::Order.count
@@ -15,5 +15,23 @@ class Boutique::ProductsControllerTest < Boutique::ControllerTest
 
     assert_equal 1, Boutique::Order.count
     assert_equal 1, Boutique::Order.first.line_items.first.amount
+  end
+
+  test "show - social media crawler gets rendered template with meta tags" do
+    product = create(:boutique_product, title: "Title",
+                                        meta_title: "Meta Title",
+                                        meta_description: "Meta Description",
+                                        og_title: "OG Title")
+
+    get product_url(product), headers: { "HTTP_USER_AGENT" => "facebookexternalhit/1.1" }
+
+    assert_response :success
+    assert_includes response.body, product.title
+    assert_includes response.body, product.meta_title
+    assert_includes response.body, product.meta_description
+    assert_includes response.body, product.og_title
+
+    # verify no order was created (no redirect happened)
+    assert_equal 0, Boutique::Order.count
   end
 end
